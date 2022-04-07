701df85c33940df3ee962873d1e47d2e9e0714a6
IsraelDCastro <fulanito203@gmail.com>
2022-04-07 10:40:26 -0400

    Adding more options between VueJS and ReactJS

--- template.rb
+++ template.rb
@@ -1,16 +1,18 @@
-require "fileutils"
-require "shellwords"
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'shellwords'
 
 def add_template_repository_to_source_path
   if __FILE__ =~ %r{\Ahttps?://}
-    require "tmpdir"
-    source_paths.unshift(tempdir = Dir.mktmpdir("rails-"))
+    require 'tmpdir'
+    source_paths.unshift(tempdir = Dir.mktmpdir('rails-'))
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
-      "--quiet",
-      "https://github.com/IsraelDCastro/rails-vite-tailwindcss-template.git",
+      '--quiet',
+      'https://github.com/IsraelDCastro/rails-vite-tailwindcss-template.git',
       tempdir
-    ].map(&:shellescape).join(" ")
+    ].map(&:shellescape).join(' ')
 
     if (branch = __FILE__[%r{rails-vite-tailwindcss-template/(.+)/template.rb}, 1])
       Dir.chdir(tempdir) { git checkout: branch }
@@ -23,16 +25,16 @@ end
 def add_gems
   gem 'vite_rails'
   gem 'vite_ruby'
-  gem "image_processing", "~> 1.2"
-  gem "annotate", group: :development
+  gem 'image_processing', '~> 1.2'
+  gem 'annotate', group: :development
 end
 
 def set_application_name
   # Add Application Name to Config
-  environment "config.application_name = Rails.application.class.module_parent_name"
+  environment 'config.application_name = Rails.application.class.module_parent_name'
 
   # Announce the user where they can change the application name in the future.
-  puts "You can change application name inside: ./config/application.rb"
+  puts 'You can change application name inside: ./config/application.rb'
 end
 
 def add_vite
@@ -40,28 +42,33 @@ def add_vite
 end
 
 def add_javascript
-  run "yarn add autoprefixer postcss sass tailwindcss vite vue"
-  run "yarn add -D @vitejs/plugin-vue @vue/compiler-sfc eslint eslint-plugin-tailwindcss eslint-plugin-vue path vite-plugin-full-reload vite-plugin-ruby"
+  run 'yarn add autoprefixer postcss sass tailwindcss vite vue'
+  run 'yarn add -D @vitejs/plugin-vue @vue/compiler-sfc eslint eslint-plugin-tailwindcss eslint-plugin-vue path vite-plugin-full-reload vite-plugin-ruby'
 end
 
 def copy_templates
-  # remove_file "app/frontend/entrypoints/application.js"
-  # remove_file "app/frontend/components" # Webpack
 
-  copy_file "Procfile.dev"
-  copy_file "jsconfig.json"
-  copy_file "tailwind.config.js"
-  copy_file "postcss.config.js"
-  copy_file "vite.config.ts"
+  copy_file 'Procfile.dev'
+  copy_file 'jsconfig.json'
+  copy_file 'tailwind.config.js'
+  copy_file 'postcss.config.js'
+  copy_file 'vite.config.ts'
 
-  directory "app", force: true
-  directory "config", force: true
-  directory "lib", force: true
+  directory 'app', force: true
+  directory 'config', force: true
+  directory 'lib', force: true
 end
 
 def add_pages_controller
-  generate "controller Pages home"
+  generate 'controller Pages home'
   route "root to: 'pages#home'"
+end
+
+def run_command_flags
+  ARGV.each do |flag|
+    # process arguments like so
+    say 'Please add VueJS', :brown if flag == '--vue'
+  end
 end
 
 # Main setup
@@ -73,33 +80,33 @@ after_bundle do
   set_application_name
   add_pages_controller
   add_javascript
+  run_command_flags
 
   copy_templates
   add_vite
-  
-  rails_command "db:create"
-  rails_command "active_storage:install"
 
-  remove_file "app/views/pages/home.html.erb"
+  rails_command 'db:create'
+  rails_command 'active_storage:install'
 
-  copy_file "app/views/pages/home.html.erb"
+  remove_file 'app/views/pages/home.html.erb'
 
-  # rails_command "db:migrate"
-  # Commit everything to git
+  copy_file 'app/views/pages/home.html.erb'
+
+
   begin
-    git add: (".")
+    git add: '.'
     git commit: %( -m 'Initial commit' )
   rescue StandardError => e
     puts e.message
   end
 
   say
-  say "Rails 7 + Vue 3 + ViteJS + Tailwindcss created!", :green
+  say 'Rails 7 + Vue 3 + ViteJS + Tailwindcss created!', :green
   say
-  say "To get started with your new app:", :yellow
+  say 'To get started with your new app:', :yellow
   say "  cd #{app_name}"
   say
-  say "  #Please update config/database.yml with your database credentials"
+  say '  #Please update config/database.yml with your database credentials'
   say
-  say "  rails s"
+  say '  rails s'
 end
