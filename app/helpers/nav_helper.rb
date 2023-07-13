da59cc273eb1b134b1b3a414ba10a8a87b80057b
IsraelDCastro <fulanito203@gmail.com>
2023-07-13 18:18:27 -0400

    nav_link_to helper added to Tailwind CSS template, and some changes on navbar.

---
+++ app/helpers/nav_helper.rb
@@ -0,0 +1,20 @@
+module NavHelper
+  def nav_link_to(name = nil, options = {}, html_options = {}, &block)
+    name, options, html_options = block, name, options if block
+
+    url = url_for(options)
+    starts_with = html_options.delete(:starts_with)
+    html_options[:class] ||= []
+    active_class = html_options.delete(:active_class) || ' is-active'
+    inactive_class = html_options.delete(:inactive_class) || ''
+
+    active = Array.wrap(starts_with).any? { |path| request.path.start_with?(path) } || request.path == url
+    html_options[:class] << (active ? " #{active_class}" : " #{inactive_class}")
+
+    html_options.except!(:class) if html_options[:class].empty?
+
+    link_to(name, url, html_options, &block)
+  end
+
+
+end
